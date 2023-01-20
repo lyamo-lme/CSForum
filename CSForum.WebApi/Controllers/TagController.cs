107a23972a000e01386ba3a658f15c9638efd251
Maks Limont <ki212_lme@student.ztu.edu.ua>
2023-01-20 20:04:33 +0200

    fixed tag repository and added tag controller with crud operations

---
+++ CSForum.WebApi/Controllers/TagController.cs
@@ -0,0 +1,74 @@
+﻿using CSForum.Core.IRepositories;
+using CSForum.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSForum.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/tags")]
+    public class TagController : Controller
+    {
+        
+        private readonly ITagRepository TagRepository;
+
+        public TagController(ITagRepository postRepository)
+        {
+            TagRepository = postRepository;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Tag>> CreatePost([FromBody] Tag model)
+        {
+            try
+            {
+                var post = await TagRepository.CreateAsync(model);
+                await TagRepository.SaveChangesAsync();
+                return Ok(post);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tag>> EditPost([FromBody] Tag model)
+        {
+            try
+            {
+                var post = await TagRepository.UpdateAsync(model);
+                await TagRepository.SaveChangesAsync();
+                return Ok(post);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e);
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<Tag>> GetPost(int postId)
+        {
+            try
+            {
+                return Ok(await TagRepository.FindAsync(x => x.Id == postId));
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<bool>> DeletePost(int postId)
+        {
+            try
+            {
+                var state = await TagRepository.DeleteAsync(postId);
+                return Ok(state);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e);
+            }
+        }
+    }
+}
